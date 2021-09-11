84fe42ae46655654dff6e08d89fbf15060b897fe
Eevi Bengs <eevi.bengs@helsinki.fi>
2021-09-11 18:25:38 +0300

    New maps.

--- Pathfinding/src/main/java/Pathfinding/Main.java
+++ Pathfinding/src/main/java/Pathfinding/Main.java
@@ -26,24 +26,24 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         MapReader map = new MapReader();
-        Graph Berlin_2_1024 = map.createGraph(new File("Berlin_2_1024.map"));
-        ScenReader Berlin_2_1024_scen = new ScenReader(new File("Berlin_2_1024.map.scen"));
+        Graph Berlin_2_1024 = map.createGraph(new File("Denver_2_1024.map"));
+        ScenReader Berlin_2_1024_scen = new ScenReader(new File("Denver_2_1024.map.scen"));
         PerformanceTests paths_Berlin_2_1024 = new PerformanceTests(Berlin_2_1024_scen.getScens(), Berlin_2_1024);
-        paths_Berlin_2_1024.run(10);
-        
-        Graph Berlin_1_512 = map.createGraph(new File("Berlin_1_512.map"));
-        ScenReader Berlin_1_512_scen = new ScenReader(new File("Berlin_1_512.map.scen"));
-        Berlin_1_512.putStartPoint(0, 0);
-        Berlin_1_512.putEndPoint(500, 500);
-        Dijkstra dij = new Dijkstra(Berlin_1_512);
-        JPS jps = new JPS(Berlin_1_512);
-        dij.findRoute();
-        jps.shortestPath();
-        dij.drawRoute();
-        System.out.println(dij.routeDistance);
-        jps.drawRoute();
-        System.out.println(Berlin_1_512);
-        
+        paths_Berlin_2_1024.run(1000);
+//        
+//        Graph Berlin_1_512 = map.createGraph(new File("Berlin_1_512.map"));
+//        ScenReader Berlin_1_512_scen = new ScenReader(new File("Berlin_1_512.map.scen"));
+//        Berlin_1_512.putStartPoint(0, 0);
+//        Berlin_1_512.putEndPoint(500, 500);
+//        Dijkstra dij = new Dijkstra(Berlin_1_512);
+//        JPS jps = new JPS(Berlin_1_512);
+//        dij.findRoute();
+//        jps.shortestPath();
+//        dij.drawRoute();
+//        System.out.println(dij.routeDistance);
+//        jps.drawRoute();
+//        System.out.println(Berlin_1_512);
+//        
         
 
     }
